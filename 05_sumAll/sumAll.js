201df61ddf44511a1b62a8cc869d6769fa8d1c2b
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-25 15:53:05 +0900

    chore(05_ex): write sumAll function
    function allows:
    - sums numbers within the range
    - works with large numbers
    - returns ERROR with negative numbers
    - returns ERROR with non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+  if (args[0] > args[1]) args.reverse();
 
+  switch (true) {
+    case args[0] < 0 || args[1] < 0:
+    case typeof args[0] !== "number" || typeof args[1] !== "number":
+      return "ERROR";
+  }
+
+  return args.reduce((firstNum, secondNum) => {
+    let sum = 0;
+
+    while (firstNum <= secondNum) {
+      sum += firstNum;
+
+      firstNum++;
+    }
+
+    return sum;
+  });
 };
 
 // Do not edit below this line
