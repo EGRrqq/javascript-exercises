482d9cb197c743dfea9a09c7162ee833e74e6922
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-25 20:58:05 +0900

    chore(07_ex): write tempConversion function
    function allows:
    - convert temperatures from Fahrenheit to Celsius, and vice versa
    - rounds to 1 decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  const conversionFormula = ((fahrenheitTemp - 32) * 5) / 9;
+
+  return Math.round(conversionFormula * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemp) {
+  const conversionFormula = (celsiusTemp * 9) / 5 + 32;
+
+  return Math.round(conversionFormula * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
