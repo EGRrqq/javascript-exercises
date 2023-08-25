1a964750bdbcc39289c887d18abdbbe7dc01b9a1
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-25 12:01:28 +0900

    chore(02_ex): write repeatString function
    function allows:
    - repeats the string many times
    - returns ERROR with negative numbers
    - works with blank strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+const repeatString = function (message, repeatNum) {
+  let repeatMessage = '';
+  let i = 0;
 
+  do {
+    switch (true) {
+      case repeatNum === 0:
+        repeatMessage = "";
+        break;
+      case repeatNum < 0:
+        repeatMessage = "ERROR";
+        break;
+      default:
+        repeatMessage = repeatMessage.concat(message);
+        break;
+    }
+
+    i++
+  } while (i < repeatNum);
+
+  return repeatMessage;
 };
 
 // Do not edit below this line
