ca8538774002b11b8287db8e5b6255f882c645be
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-29 07:58:03 +0900

    do(ex_09): make a palindrome check function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const clearStrArr = str.toLowerCase().match(/[a-z0-9]/g);
 
+  return clearStrArr.join("") === clearStrArr.reverse().join("");
 };
 
 // Do not edit below this line
