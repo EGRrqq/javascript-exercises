7061d6050b7f1efddca7878eccb2a2b8d14c5899
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-29 18:11:16 +0900

    chore(12_ex): create findTheOldest function
    findTheOldest function:
    - accepts an array of objects representing people
    - calculate the age of each person
    - sort from oldest to youngest
    - returns the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  people.reduce((acc, item) => {
+    if (item.yearOfDeath) {
+      acc = item.yearOfDeath;
+    } else {
+      acc = new Date().getFullYear();
+    }
 
+    return (item.age = acc - item.yearOfBirth);
+  }, 0);
+
+  return people.sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
