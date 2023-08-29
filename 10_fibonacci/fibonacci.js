d289b01dca3c3c10dff5f107b78e15577a5defe5
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-08-29 11:09:38 +0900

    chore(10_ex): Create a Fibonacci function
    function returns a specific member of the Fibonacci sequence
    - without memoization

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+// basic iteration, don't care about memory
+// - base cases
+// - loop
+// - return arr[num]
+// iteration with memo
 
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let arr = [0, 1];
+
+  for (let i = arr.length; i <= num; i++) {
+    arr.push(arr[i - 2] + arr[i - 1]);
+  }
+
+  return arr[num];
 };
 
 // Do not edit below this line
